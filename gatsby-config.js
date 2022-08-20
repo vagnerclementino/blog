9f90cb1686a0d7be7d5eb10131cfd46c793cc0c1
Vagner Clementino <76048847+vagnerclementino-hotmart@users.noreply.github.com>
2022-08-20 11:03:19 -0300

    feat: :sparkles: add rss support (#37)

--- gatsby-config.js
+++ gatsby-config.js
@@ -168,6 +168,59 @@ module.exports = {
         timezone: 'America/Sao_Paulo',
         force: process.env.NODE_ENV === 'development'
       }
-    }
+    },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMdx } }) => {
+              return allMdx.nodes.map(node => {
+                return Object.assign({}, node.frontmatter, {
+                  description: node.excerpt,
+                  date: node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + node.fields.slug,
+                  custom_elements: [{ "content:encoded": node.html }],
+                })
+              })
+            },
+            query: `
+              {
+                allMdx(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  nodes {
+                    excerpt
+                    html
+                    fields { 
+                      slug 
+                    }
+                    frontmatter {
+                      title
+                      date
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "Clementino Notes' RSS Feed",
+          },
+        ],
+      },
+    },
   ],
 }
