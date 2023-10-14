1e041d5e63249b6aca4d6249b2fc91fc6a5c9e62
Vagner Clementino <vagner.clementino@hotmart.com>
2023-10-14 17:58:17 -0300

    chore(gatsby): :heavy_minus_sign: remove gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -135,19 +135,6 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        // edit below
-        icon: `content/assets/clementine-icon.png`,
-      },
-    },
-    {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography`,
