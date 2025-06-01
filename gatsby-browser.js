b91b71dcd02ce5f3b00f57d630b12490b0177148
Vagner Clementino <vagner.clementino@hotmart.com>
2025-06-01 15:57:25 -0300

    add primis-js support

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,3 +2,7 @@
 import "typeface-montserrat"
 import "typeface-merriweather"
 import "./src/styles/global.css"
+
+require("prismjs/themes/prism-solarizedlight.css")
+require("prismjs/plugins/line-numbers/prism-line-numbers.css")
+require("prismjs/plugins/command-line/prism-command-line.css")
