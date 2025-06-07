f9669bb935cf5b1a85c4365c52abc2b9c21df67b
Vagner Clementino <vagner.clementino@hotmart.com>
2025-06-07 20:49:54 -0300

    refactor: convert require to import statements

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
-require("prismjs/themes/prism-solarizedlight.css")
-require("prismjs/plugins/line-numbers/prism-line-numbers.css")
-require("prismjs/plugins/command-line/prism-command-line.css")
+import "prismjs/themes/prism-solarizedlight.css"
+import "prismjs/plugins/line-numbers/prism-line-numbers.css"
+import "prismjs/plugins/command-line/prism-command-line.css"
 
 import "typeface-montserrat"
 import "typeface-merriweather"
