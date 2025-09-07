771f3101fb816d5032310b078198de5aeba6769e
Vagner Clementino <vagner.clementino@gmail.com>
2025-09-07 11:37:35 -0300

    Update .eslintrc.js
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
   ],
   plugins: ["react", "react-hooks", "jsx-a11y"],
   parserOptions: {
