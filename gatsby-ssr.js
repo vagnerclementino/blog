c96d2d3ee8e097bcd0cd9bb2b179941e04daa4ee
Vagner Clementino <vagner.clementino@gmail.com>
2026-05-04 10:58:36 -0300

    chore(linter): changes after linter erros

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,5 +1,7 @@
-// Font Awesome: desabilitar injeção automática de CSS no SSR
-// O CSS é importado via gatsby-browser.js e incluído no bundle estático
-// Isso garante que os ícones SVG já tenham dimensões corretas no HTML inicial
+/*
+ * Font Awesome: desabilitar injeção automática de CSS no SSR.
+ * O CSS é importado via gatsby-browser.js e incluído no bundle estático.
+ * Isso garante que os ícones SVG já tenham dimensões corretas no HTML inicial.
+ */
 import { config } from "@fortawesome/fontawesome-svg-core"
 config.autoAddCss = false
