60b0b78689e79db02b4d6e7f114f4c8ca16954ea
Vagner Clementino <vagner.clementino@gmail.com>
2026-05-04 11:34:07 -0300

    fix: corrigir FOUC dos ícones Font Awesome no carregamento da home (#150)
    * fix(home): allow best performance load
    
    * chore(linter): changes after linter erros
    
    * chore(linter): remove comments

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,7 @@
+import { config } from "@fortawesome/fontawesome-svg-core"
+import "@fortawesome/fontawesome-svg-core/styles.css"
+config.autoAddCss = false
+
 import "./src/styles/prism-dracula.css"
 import "prismjs/plugins/line-numbers/prism-line-numbers.css"
 import "prismjs/plugins/command-line/prism-command-line.css"
