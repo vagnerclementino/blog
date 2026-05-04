95368042a4e8aed55ad56300112d58acd5bd6985
Vagner Clementino <vagner.clementino@gmail.com>
2026-05-04 10:50:38 -0300

    fix(home): allow best performance load

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,10 @@
+// Font Awesome: desabilitar injeção automática de CSS e importar manualmente
+// Isso evita o FOUC (Flash of Unstyled Content) onde os ícones SVG aparecem gigantes
+// antes do JavaScript carregar e injetar o CSS
+import { config } from "@fortawesome/fontawesome-svg-core"
+import "@fortawesome/fontawesome-svg-core/styles.css"
+config.autoAddCss = false
+
 import "./src/styles/prism-dracula.css"
 import "prismjs/plugins/line-numbers/prism-line-numbers.css"
 import "prismjs/plugins/command-line/prism-command-line.css"
---
+++ gatsby-ssr.js
@@ -0,0 +1,5 @@
+// Font Awesome: desabilitar injeção automática de CSS no SSR
+// O CSS é importado via gatsby-browser.js e incluído no bundle estático
+// Isso garante que os ícones SVG já tenham dimensões corretas no HTML inicial
+import { config } from "@fortawesome/fontawesome-svg-core"
+config.autoAddCss = false
